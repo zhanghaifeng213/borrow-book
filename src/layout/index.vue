4fc0d647aa95bf57fdbd42df8b489c6ddff64e4f
zhanghf <751434992@qq.com>
2019-07-30 18:57:12 +0800

    qq

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,13 +1,6 @@
 <template>
   <div :class="classObj" class="app-wrapper">
-    <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
-    <sidebar class="sidebar-container" />
-    <div class="main-container">
-      <div :class="{'fixed-header':fixedHeader}">
-        <navbar />
-      </div>
-      <app-main />
-    </div>
+    
   </div>
 </template>
 
